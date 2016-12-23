fb3a4414c8dea22a437d2094f5268a867374aceb
Nathan Parker <nathan@udundi.com>
2016-12-23 06:44:33 -0600

    implementing multi page http req - restarting from scratch

--- server/api/amazon/index.js
+++ server/api/amazon/index.js
@@ -1,44 +1,29 @@
-var app          = require('../../../app.js');
-var _            = require('lodash');
-var url          = require('url');
-var fs           = require('fs');
-var async        = require('async');
-var request      = require('request');
-var cheerio      = require('cheerio');
-var data         = {};
-    data.headers = {};
-    data.json    = true;
+var app         = require('../../../app.js');
+var url         = require('url');
+var querystring = require('querystring');
+var request     = require('request');
+var cheerio     = require('cheerio');
+var scriptAdded = false;
 
-// function requestData(data, callback){
-//   request(data, function (error, response, body) {
-//     if (!error && response.statusCode == 200) {
-//       callback(null, _.values(body));
-//     }
-//   });
-// }
+// var app          = require('../../../app.js');
+// var url          = require('url');
+// var querystring = require('querystring');
+// var fs           = require('fs');
+// var async        = require('async');
+// var request      = require('request');
+// var cheerio      = require('cheerio');
 
-// function handleError(res, err) {
-//   return res.status(500).send(err);
-// }
 
-exports.getReviews = function(req, res) {
+exports.getReviews = function(req, res){
   var merchantId = req.query['merchantId'];
   url = 'https://www.amazon.com/s?me='+merchantId;
-  // url2 = url+'&page=2';
 
   request(url, function(error, results, html){
     if(!error){
       var $ = cheerio.load(html);
-      
-      var breadcrumb = $('#main').find('#s-result-count').text();
-      var limit = breadcrumb.match(/-([^\s]+)/)[1];
-      var count = breadcrumb.match(/of\s([^\s]+)/)[1];
-
-      var pages = Math.ceil(count/limit);
-      console.log(pages);
-
+      console.time('scraper');
       var products = [];
-      $('.s-result-item').each(function(i, elem) {
+      $('.s-result-item').each(function(i, elem){
         var data = $(this);
         var dataItem = data.children().find('.s-access-detail-page');
         var dataReviews = data.children().last().children().last('a').text().split('\n\n');
@@ -54,26 +39,28 @@ exports.getReviews = function(req, res) {
       });
     }
     res.status(200).send(products);
+    console.timeEnd('scraper');
   })
 }
 
-// async.map(['url1', 'url2'], function(item, callback){
-//   request(item, function (error, response, body) {
-//     callback(error, body);
-//   });
-// }, function(err, results){
-//   console.log(results);
-// });
-
-// exports.getReview = function(req, res) {
+// exports.getReviews = function(req, res){
 //   var merchantId = req.query['merchantId'];
 //   url = 'https://www.amazon.com/s?me='+merchantId;
+//   // url2 = url+'&page=2';
 
 //   request(url, function(error, results, html){
 //     if(!error){
 //       var $ = cheerio.load(html);
+      
+//       // var breadcrumb = $('#main').find('#s-result-count').text();
+//       // var limit = breadcrumb.match(/-([^\s]+)/)[1];
+//       // var count = breadcrumb.match(/of\s([^\s]+)/)[1];
+
+//       // var pages = Math.ceil(count/limit);
+//       // console.log(pages);
+
 //       var products = [];
-//       $('.s-result-item').each(function(i, elem) {
+//       $('.s-result-item').each(function(i, elem){
 //         var data = $(this);
 //         var dataItem = data.children().find('.s-access-detail-page');
 //         var dataReviews = data.children().last().children().last('a').text().split('\n\n');
@@ -91,3 +78,11 @@ exports.getReviews = function(req, res) {
 //     res.status(200).send(products);
 //   })
 // }
+
+// async.map(['url1', 'url2'], function(item, callback){
+//   request(item, function (error, response, body) {
+//     callback(error, body);
+//   });
+// }, function(err, results){
+//   console.log(results);
+// });
