25a904eca683717a1a439ccda8f193dbd918454d
Nathan Parker <nathan@udundi.com>
2016-12-24 10:25:33 -0600

    created a loop that scraps multiple pages with single call

--- api/amazon/index.js
+++ api/amazon/index.js
@@ -2,129 +2,74 @@ var app         = require('../../app.js');
 var url         = require('url');
 var querystring = require('querystring');
 var phantom     = require('phantom');
-// var https       = require('http');
-// var request     = require('request');
 var cheerio     = require('cheerio');
 
 
 exports.getReviews = function(req, res){
-  console.time('scraper');
-
   var merchantId = req.query['merchantId'];
   console.log('-------MERCHANT-ID--------');
   var url = 'https://www.amazon.com/s?me='+merchantId;
   var _ph, _page, _outObj;
+  var paginate = null;
+  var pages = null;
+  var body = [];
 
-  phantom.create(['--ignore-ssl-errors=yes', '--load-images=no']).then(function(ph){
-    _ph = ph;
-    return _ph.createPage();
-  }).then(function(page){
-    _page = page;
-    return _page.open(url);
-  }).then(function(status){
-    console.log(status);
-    return _page.evaluate(function(){
-      return document.getElementById('s-results-list-atf').innerHTML;
-    });
-  }).then(function(html){
-    var $ = cheerio.load(html);
-    var data = [];
-    $('.s-result-item').each(function(i, elem){
-      var dataItem = $(elem).children().find('.s-access-detail-page');
-      var dataReviews = $(elem).children().last().children().last('a').text().split('\n\n');
-      data.push({
-        'id': $(elem).attr('data-asin'),
-        'handle': dataItem.attr('href').split('/')[3],
-        'title': dataItem.attr('title'),
-        'reviews': {
-          'totals': dataReviews[1],
-          'average': dataReviews[0].trim().replace(/( out of 5 stars)/i, '')
-        }
+  function pushData(data){
+    body.push(data);
+  };
+
+  function sendData(data){
+    body.push(data);
+    res.status(200).send(body);
+  };
+
+  (function pageinate(i){
+    phantom.create(['--ignore-ssl-errors=yes', '--load-images=no']).then(function(ph){
+      _ph = ph;
+      return _ph.createPage();
+    }).then(function(page){
+      _page = page;
+      return _page.open(url+'&page='+i);
+    }).then(function(status){
+      return _page.evaluate(function(){
+        return document.getElementById('s-results-list-atf').innerHTML;
       });
+    }).then(function(html){
+      var $ = cheerio.load(html);
+      if(pageinate == null){
+        var breadcrumb = $('#main').find('#s-result-count').text();
+        var limit = breadcrumb.match(/-([^\s]+)/)[1];
+        var count = breadcrumb.match(/of\s([^\s]+)/)[1];
+        pages = Math.ceil(count/limit);
+      }
+      var data = [];
+      $('.s-result-item').each(function(i, elem){
+        var dataItem = $(elem).children().find('.s-access-detail-page');
+        var dataReviews = $(elem).children().last().children().last('a').text().split('\n\n');
+        data.push({
+          'id': $(elem).attr('data-asin'),
+          'handle': dataItem.attr('href').split('/')[3],
+          'title': dataItem.attr('title'),
+          'reviews': {
+            'totals': dataReviews[1],
+            'average': dataReviews[0].trim().replace(/( out of 5 stars)/i, '')
+          }
+        });
+      });
+      if(i===null){
+        i = 2;
+        pageinate(i);
+        return pushData(data);
+      }else if(i<pages){
+        pageinate(i);
+        return pushData(data);
+      }else{
+        return sendData(data);
+      }
+      _page.close();
+      _ph.exit();
+    }).catch(function(e){
+      console.log(e);
     });
-    res.status(200).send(data);
-    _page.close();
-    _ph.exit();
-  }).catch(function(e){
-    console.log(e);
-    _page.close();
-    _ph.exit();
-  });
-  console.timeEnd('scraper');
+  })(pages);
 }
-
-// exports.getReviews = function(req, res){
-//   var merchantId = req.query['merchantId'];
-//   console.log('-------MERCHANT-ID--------');
-//   var url = 'https://www.amazon.com/s?me='+merchantId;
-//   var _ph, _page, _outObj;
-
-//   phantom.create(['--ignore-ssl-errors=yes', '--load-images=no']).then(function(ph){
-//       _ph = ph;
-//       return _ph.createPage();
-//   }).then(function(page){
-//       _page = page;
-//       return _page.open(url);
-//   }).then(function(status){
-//       console.log(status);
-//       return _page.property('content');
-//   }).then(function(content){
-//       console.log(content);
-//       res.status(200).send(content);
-//       _page.close();
-//       _ph.exit();
-//   }).catch(function(e){
-//       console.log(e);
-//   });
-// };
-
-
-// exports.getReviews = function(req, res){
-//   var merchantId = req.query['merchantId'];
-//   console.log('-------MERCHANT-ID--------');
-//   url = 'https://www.amazon.com/s?me='+merchantId;
-
-  // request(url, function(error, results, html){
-  //   if(!error){
-  //     var $ = cheerio.load(html);
-      // console.time('scraper');
-
-      // var breadcrumb = $('#main').find('#s-result-count').text();
-      // var limit = breadcrumb.match(/-([^\s]+)/)[1];
-      // var count = breadcrumb.match(/of\s([^\s]+)/)[1];
-
-      // var pages = Math.ceil(count/limit);
-      // console.log(pages);
-
-      // async.map(['url1', 'url2'], function(item, callback){
-      //   request(item, function (error, response, body) {
-      //     callback(error, body);
-      //   });
-      // }, function(err, results){
-      //   console.log(results);
-      // });
-
-    //   var products = [];
-    //   $('.s-result-item').each(function(i, elem){
-    //     var data = $(this);
-    //     var dataItem = data.children().find('.s-access-detail-page');
-    //     var dataReviews = data.children().last().children().last('a').text().split('\n\n');
-    //     products.push({
-    //       'id': data.attr('data-asin'),
-    //       'handle': dataItem.attr('href').split('/')[3],
-    //       'title': dataItem.attr('title'),
-    //       'reviews': {
-    //         'totals': dataReviews[1],
-    //         'average': dataReviews[0].trim().replace(/( out of 5 stars)/i, '')
-    //       }
-    //     });
-    //   });
-    //   res.status(200).send(html);
-    // }
-    // res.status(400).send(error);
-    // console.log('-------PRODUCTS--------');
-    // console.log(products);
-    
-    // console.timeEnd('scraper');
-  // })
-// }
