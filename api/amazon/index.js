b2b369649961584776bf02cae2d44bb5fe9c79f0
Nathan Parker <nathan@udundi.com>
2016-12-23 21:30:37 -0600

    using phantomjs to circumvent amazon web scraping captcha

--- api/amazon/index.js
+++ api/amazon/index.js
@@ -1,19 +1,92 @@
 var app         = require('../../app.js');
 var url         = require('url');
 var querystring = require('querystring');
-var request     = require('request');
+var phantom     = require('phantom');
+// var https       = require('http');
+// var request     = require('request');
 var cheerio     = require('cheerio');
-// var scriptAdded = false;
 
-exports.getReviews = function(req, res) {
+
+exports.getReviews = function(req, res){
+  console.time('scraper');
+
   var merchantId = req.query['merchantId'];
   console.log('-------MERCHANT-ID--------');
-  console.log(merchantId);
-  url = 'https://www.amazon.com/s?me='+merchantId;
+  var url = 'https://www.amazon.com/s?me='+merchantId;
+  var _ph, _page, _outObj;
 
-  request(url, function(error, results, html){
-    if(!error){
-      var $ = cheerio.load(html);
+  phantom.create(['--ignore-ssl-errors=yes', '--load-images=no']).then(function(ph){
+    _ph = ph;
+    return _ph.createPage();
+  }).then(function(page){
+    _page = page;
+    return _page.open(url);
+  }).then(function(status){
+    console.log(status);
+    return _page.evaluate(function(){
+      return document.getElementById('s-results-list-atf').innerHTML;
+    });
+  }).then(function(html){
+    var $ = cheerio.load(html);
+    var data = [];
+    $('.s-result-item').each(function(i, elem){
+      var dataItem = $(elem).children().find('.s-access-detail-page');
+      var dataReviews = $(elem).children().last().children().last('a').text().split('\n\n');
+      data.push({
+        'id': $(elem).attr('data-asin'),
+        'handle': dataItem.attr('href').split('/')[3],
+        'title': dataItem.attr('title'),
+        'reviews': {
+          'totals': dataReviews[1],
+          'average': dataReviews[0].trim().replace(/( out of 5 stars)/i, '')
+        }
+      });
+    });
+    res.status(200).send(data);
+    _page.close();
+    _ph.exit();
+  }).catch(function(e){
+    console.log(e);
+    _page.close();
+    _ph.exit();
+  });
+  console.timeEnd('scraper');
+}
+
+// exports.getReviews = function(req, res){
+//   var merchantId = req.query['merchantId'];
+//   console.log('-------MERCHANT-ID--------');
+//   var url = 'https://www.amazon.com/s?me='+merchantId;
+//   var _ph, _page, _outObj;
+
+//   phantom.create(['--ignore-ssl-errors=yes', '--load-images=no']).then(function(ph){
+//       _ph = ph;
+//       return _ph.createPage();
+//   }).then(function(page){
+//       _page = page;
+//       return _page.open(url);
+//   }).then(function(status){
+//       console.log(status);
+//       return _page.property('content');
+//   }).then(function(content){
+//       console.log(content);
+//       res.status(200).send(content);
+//       _page.close();
+//       _ph.exit();
+//   }).catch(function(e){
+//       console.log(e);
+//   });
+// };
+
+
+// exports.getReviews = function(req, res){
+//   var merchantId = req.query['merchantId'];
+//   console.log('-------MERCHANT-ID--------');
+//   url = 'https://www.amazon.com/s?me='+merchantId;
+
+  // request(url, function(error, results, html){
+  //   if(!error){
+  //     var $ = cheerio.load(html);
       // console.time('scraper');
 
       // var breadcrumb = $('#main').find('#s-result-count').text();
@@ -31,26 +104,27 @@ exports.getReviews = function(req, res) {
       //   console.log(results);
       // });
 
-      var products = [];
-      $('.s-result-item').each(function(i, elem){
-        console.log(elem);
-        var data = $(this);
-        var dataItem = data.children().find('.s-access-detail-page');
-        var dataReviews = data.children().last().children().last('a').text().split('\n\n');
-        products.push({
-          'id': data.attr('data-asin'),
-          'handle': dataItem.attr('href').split('/')[3],
-          'title': dataItem.attr('title'),
-          'reviews': {
-            'totals': dataReviews[1],
-            'average': dataReviews[0].trim().replace(/( out of 5 stars)/i, '')
-          }
-        });
-      });
-    }
+    //   var products = [];
+    //   $('.s-result-item').each(function(i, elem){
+    //     var data = $(this);
+    //     var dataItem = data.children().find('.s-access-detail-page');
+    //     var dataReviews = data.children().last().children().last('a').text().split('\n\n');
+    //     products.push({
+    //       'id': data.attr('data-asin'),
+    //       'handle': dataItem.attr('href').split('/')[3],
+    //       'title': dataItem.attr('title'),
+    //       'reviews': {
+    //         'totals': dataReviews[1],
+    //         'average': dataReviews[0].trim().replace(/( out of 5 stars)/i, '')
+    //       }
+    //     });
+    //   });
+    //   res.status(200).send(html);
+    // }
+    // res.status(400).send(error);
     // console.log('-------PRODUCTS--------');
     // console.log(products);
-    res.status(200).send(products);
+    
     // console.timeEnd('scraper');
-  })
-}
+  // })
+// }
--- app.js
+++ app.js
@@ -4,7 +4,6 @@ var fs        = require('fs');
 var amazon    = require('./api/amazon');
 var morgan    = require('morgan');
 var parser    = require('body-parser');
-var useragent = require('express-useragent');
 var logger    = morgan('combined');
 var app       = express();
 
@@ -21,43 +20,20 @@ app.use(function(req, res, next) {
   console.log(app.locals);
   console.log('------END_HEADERS-------');
   res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,PATCH,OPTIONS');
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   // res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
-  res.locals.ua = req.get('User-Agent');
   next();
 });
-
-// headers = {
-//     "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
-//     "Accept-Encoding": "gzip, deflate, sdch, br",
-//     "Accept-Language": "en-US,en;q=0.8",
-//     "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36",
-// }
 
 app.get('/amazon/getReviews', amazon.getReviews);
 
 // [START server]
-// var server = app.listen(process.env.PORT || 5000, function () {
-//   var host = server.address().address;
-//   var port = server.address().port;
-
-//   console.log('App listening at http://localhost:'+port);
-// });
-
-var portrange = 45032;
-function getPort(){
-  var port = portrange;
-  portrange += 1;
-}
-
-var server = app.listen(process.env.PORT || getPort(), function(){
+var server = app.listen(process.env.PORT || 8080, function () {
   var host = server.address().address;
   var port = server.address().port;
 
   console.log('App listening at http://localhost:'+port);
 });
-
 // [END server]
 
 exports = module.exports = app;
