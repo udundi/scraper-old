c98afe3eff7c8f7f6e07e8c0ae053d676da0ec5b
Nathan Parker <nathan@udundi.com>
2018-07-18 10:30:09 -0500

    edited to instagram scraper to work with facebook ograph

--- api/instagram/index.js
+++ api/instagram/index.js
@@ -35,24 +35,27 @@ exports.getUser = function(req, res){
   }).then(function(data) {
     var user = data.entry_data.ProfilePage[0].graphql.user;
     var data = user.edge_owner_to_timeline_media.edges;
-    _maxId = _.last(data).id
-    
-    return pushData(data);
+    // _maxId = _.last(data).id;
 
-    _page.close();
-  }).then(function() {
-    return _page.open(url+'/?max_id='+_maxId);
-  }).then(function(status) {
-    return _page.evaluate(function() {
-      return document.defaultView._sharedData;
-    });
-  }).then(function(data) {
-    // var user = _.pluck(data.entry_data.ProfilePage, 'user');
-    // var data = user[0]['media'].nodes;
-    var user = data.entry_data.ProfilePage[0].graphql.user;
-    var data = user.edge_owner_to_timeline_media.edges;
+    // console.log(_maxId);
     
+    // return pushData(data);
     return sendData(data);
+
+  //   _page.close();
+  // }).then(function() {
+  //   return _page.open(url+'/?max_id='+_maxId);
+  // }).then(function(status) {
+  //   return _page.evaluate(function() {
+  //     return document.defaultView._sharedData;
+  //   });
+  // }).then(function(data) {
+  //   // var user = _.pluck(data.entry_data.ProfilePage, 'user');
+  //   // var data = user[0]['media'].nodes;
+  //   var user = data.entry_data.ProfilePage[0].graphql.user;
+  //   var data = user.edge_owner_to_timeline_media.edges;
+    
+  //   return sendData(data);
 
     _page.close();
     _ph.exit();
