8c9f80f9f1f80faa5a0ab917d5797b2822596c5a
Nathan Parker <nathan@udundi.com>
2018-07-26 05:06:55 -0500

    decode URI

--- api/shopify/index.js
+++ api/shopify/index.js
@@ -1,6 +1,7 @@
 var app         = require('../../app.js');
 var url         = require('url');
 var querystring = require('querystring');
+var iframeReplacement = require('../../node_modules/node-iframe-replacement/index.js');
 
 exports.getPage = function(req, res) {
   var embedUrl = req.query['embedUrl'];
