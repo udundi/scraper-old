ecc0114ecc0d4a3384a3a766bc4815cef3eff855
Nathan Parker <nathan@udundi.com>
2018-07-26 05:04:27 -0500

    decode URI

--- api/shopify/index.js
+++ api/shopify/index.js
@@ -4,12 +4,11 @@ var querystring = require('querystring');
 
 exports.getPage = function(req, res) {
   var embedUrl = req.query['embedUrl'];
-
-  console.log(embedUrl);
+  var url = decodeURIComponent(embedUrl);
 	
 	res.merge('', {
 	  // sourceUrl: 'http://www.bbc.co.uk/news',                             // external url to fetch
-	  sourceUrl: embedUrl
+	  sourceUrl: decodeURIComponent(url)
 	  // sourcePlaceholder: 'div[data-entityid="container-top-stories#1"]'   // css selector to inject our content into
 	});
 }
